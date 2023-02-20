17f3f4d30ea826864c3c9328aa163e84f3ad340c
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-02-19 20:48:53 -0600

    Added title and page icon

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ export interface LogoConstant{
     alt: string;
 }
 
-export const PAGE_TITLE: string = ' ';
+export const PAGE_TITLE: string = '';
 
 export const HEADER_ITEMS: HeaderConstant[] = [
     {
