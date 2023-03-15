b91cfc8030ffb4011a130d57d97773041ecdb163
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-03-14 19:05:36 -0600

    Fixed some issues related with post

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,14 +1,19 @@
-import React,{useState} from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import "./LoginPage.css";
-import { LINKS } from "../../constants";
 import Header from "src/Components/HeaderProp/Header";
 import Footer from "src/Components/FooterProp/Footer";
-import { HEADER_ITEMS, HeaderConstant } from '../../constants';
-import { LOGO_CONSTANTS, LogoConstant } from '../../constants';
-import {PAGE_TITLE} from '../../constants';
+import { LINKS,LOGO_CONSTANTS, LogoConstant,PAGE_TITLE,HEADER_ITEMS,HeaderConstant } from '../../constants';
 import BackgroundProp from "src/Components/BackgroundProp/BackgroundProp";
 import { useNavigate } from "react-router-dom";
 const Swal = require('sweetalert2')
+
+interface ApiResponse {
+    status: "success" | "failure";
+    token?: string;
+    username?: string;
+    id?: string;
+  }
+
 
 export const LoginPage = () => {
     const title = PAGE_TITLE;
@@ -22,62 +27,74 @@ export const LoginPage = () => {
     const [usernameLogin, setUsernameLogin] = useState("");
     const [passwordLogin, setPasswordLogin] = useState("");
 
-    const handleUsernameLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUsernameLogin(event.target.value);
-    };
-
-    const handlePasswordLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPasswordLogin(event.target.value);
-    };
+    const handleUsernameLogin = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+          setUsernameLogin(event.target.value);
+        },
+        []
+      );
+    
+      const handlePasswordLogin = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+          setPasswordLogin(event.target.value);
+        },
+        []
+      );
 
     const url = process.env.REACT_APP_API_URL;
 
-    const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (usernameLogin === "" || passwordLogin === "") {
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "Please fill in all the fields!",
-            });
+      
+        if (usernameLogin === '' || passwordLogin === '') {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please fill in all the fields!',
+          });
         } else {
-            fetch(url + "user/login",{
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Username" : usernameLogin,
-                    "psswd" : passwordLogin
-                },
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data.status === "success") {
-                        localStorage.setItem("token", data.token);
-                        localStorage.setItem("username", data.username);
-                        localStorage.setItem("id", data.id);
-                    }
-                })
-                .then(() => {
-                    if (localStorage.getItem("token") !== null) {
-                        Swal.fire({
-                            icon: "success",
-                            title: "Success!",
-                            text: "You have successfully logged in!",
-                        });
-                        navigate(LINKS.PROFILE.path);
-                    } else {
-                        Swal.fire({
-                            icon: "error",
-                            title: "Oops...",
-                            text: "Something went wrong!",
-                        });
-                    }
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+          try {
+            const response = await fetch(`${url}user/login`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                username: usernameLogin,
+                password: passwordLogin,
+              }),
+            });
+      
+            const data = await response.json();
+      
+            if (data.status === 'success') {
+              localStorage.setItem('token', data.token);
+              localStorage.setItem('username', data.username);
+              localStorage.setItem('id', data.id);
+              Swal.fire({
+                icon: 'success',
+                title: 'Success!',
+                text: 'You have successfully logged in!',
+              });
+              navigate(LINKS.PROFILE.path);
+            } else {
+              Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Invalid username or password!',
+              });
+            }
+          } catch (error) {
+            console.log(error);
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Something went wrong!',
+            });
+          }
         }
-    };
+      };
+      
 
     return(
          
