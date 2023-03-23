fbec6de6f011d5bab02ecbc16553779d7f5eb053
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-03-22 22:15:00 -0600

    HeaderComponent burger icon updated

--- src/Components/HeaderComponent/HeaderComponent.tsx
+++ src/Components/HeaderComponent/HeaderComponent.tsx
@@ -1,83 +1,87 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./HeaderC.css";
-import { LINKS,LogoConstant,HeaderConstant } from '../../constants';
+import { LINKS, LogoConstant, HeaderConstant } from '../../constants';
 
 interface MenuItem {
-    label: string;
-    path: string;
-  }
-  
-  interface HeaderProps {
-    logo: LogoConstant;
-    menuItems: HeaderConstant[];
-    isMenuOpen: boolean;
-    isAuthenticated: boolean;
-    isAdmin: boolean;
-    onMenuToggle: () => void;
-  }
-  
-  const HeaderComponent: React.FC<HeaderProps> = ({
-    menuItems,
-    logo,
-    isAuthenticated,
-    isAdmin,
-  }) => {
-    const navigate = useNavigate();
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-    const handleMenuToggle = () => {
-      setIsMenuOpen(!isMenuOpen);
-    };
-  
-    let filteredMenuItems: MenuItem[] = [];
-  
-    if (isAuthenticated) {
-      filteredMenuItems = isAdmin
+  label: string;
+  path: string;
+}
+
+interface HeaderProps {
+  logo: LogoConstant;
+  menuItems: HeaderConstant[];
+  isMenuOpen: boolean;
+  isAuthenticated: boolean;
+  isAdmin: boolean;
+  onMenuToggle: () => void;
+}
+
+const HeaderComponent: React.FC<HeaderProps> = ({
+  menuItems,
+  logo,
+  isAuthenticated,
+  isAdmin,
+}) => {
+  const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  let filteredMenuItems: MenuItem[] = [];
+
+
+  if (isAuthenticated) {
+    filteredMenuItems = isAdmin
       ? [
         //{ label: 'Logout', path: LINKS.LOGOUT.path },
         { label: 'Dashboard', path: LINKS.DASHBOARD.path },
       ] // .concat(menuItems)
       : menuItems;
-  
-    } else {
-      filteredMenuItems = [
-        //{ label: 'Dashboard', path: LINKS.DASHBOARD.path },
-        { label: 'Home', path: LINKS.HOME.path },
-        { label: 'About', path: LINKS.ABOUT.path },
-        { label: 'Login', path: LINKS.LOGIN.path },
-      ];
-    }
-  
-    return (
-        <>
-          <header className="HeaderAulify">
-            <div className="headerLogo">
-              <img
-                src={logo.path}
-                alt="Aulify Logo"
-                onClick={() => navigate(LINKS.HOME.path)}
-              />
-            </div>
-            <div className="headerMenu" onClick={handleMenuToggle}>
-              &#9776;
-            </div>
-          </header>
-          {isMenuOpen && (
-            <div className="MenuDesplegable">
-              <ul className="listaMenu">
-                {filteredMenuItems.map((item, index) => (
-                  <li className="listLi" key={index}>
-                    <a id={item.label} onClick={() => navigate(item.path)}>
-                      {item.label}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </>
-      );
-    };
-    
+
+  } else {
+    filteredMenuItems = [
+      //{ label: 'Dashboard', path: LINKS.DASHBOARD.path },
+      { label: 'Home', path: LINKS.HOME.path },
+      { label: 'About', path: LINKS.ABOUT.path },
+      { label: 'Login', path: LINKS.LOGIN.path },
+    ];
+  }
+
+  return (
+    <>
+      <header className="HeaderAulify">
+        <div className="headerLogo">
+          <img
+            src={logo.path}
+            alt="Aulify Logo"
+            onClick={() => navigate(LINKS.HOME.path)}
+          />
+        </div>
+        <div
+          className={`headerMenu ${isMenuOpen ? "active" : ""}`}
+          onClick={handleMenuToggle}
+        >
+          &#9776;
+        </div>
+      </header>
+      {isMenuOpen && (
+        <div className="MenuDesplegable">
+          <ul className={`listaMenu ${isMenuOpen ? "active" : ""}`}>
+            {filteredMenuItems.map((item, index) => (
+              <li className="listLi" key={index}>
+                <a id={item.label} onClick={() => navigate(item.path)}>
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
 export default HeaderComponent;      
