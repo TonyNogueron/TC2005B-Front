fec9b7b3c239ec683aed22d6c1ab2f1cac2efa1d
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-03-29 12:13:39 -0600

    Revert "tonypruebaestamae"
    This reverts commit 973bc6d4a3710c4e3c62f3503edee2a85a0152a2.

--- src/Components/DashboardContainer/DashboardContainer.tsx
+++ src/Components/DashboardContainer/DashboardContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback, useEffect } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import "./DashboardContainer.css";
 import { KID_STATISTICS } from "src/gameConstants";
 import Stats from "../StatsProp/Stats";
@@ -11,24 +11,24 @@ interface IStatsProps {
 
 const testResponse = [
   {
-    username: "Tony",
-    name: "Antonio Noguerón",
-    score: 800,
-    totalTimePlayed: "00:00:00",
-    completedLevels: 1,
-    totalMistakes: "12",
-    totalAttempts: "1",
+      "username": "Tony",
+      "name": "Antonio Noguerón",
+      "score": 800,
+      "totalTimePlayed": "00:00:00",
+      "completedLevels": 1,
+      "totalMistakes": "12",
+      "totalAttempts": "1"
   },
   {
-    username: "Tony2",
-    name: "Antonio Noguerón",
-    score: 673,
-    totalTimePlayed: "00:10:21",
-    completedLevels: 1,
-    totalMistakes: "10",
-    totalAttempts: "2",
-  },
-];
+      "username": "Tony2",
+      "name": "Antonio Noguerón",
+      "score": 673,
+      "totalTimePlayed": "00:10:21",
+      "completedLevels": 1,
+      "totalMistakes": "10",
+      "totalAttempts": "2"
+  }
+]
 const MemoizedStats = React.memo(function StatsComponent({
   name,
   value,
@@ -51,7 +51,6 @@ const debounce = <F extends (...args: any[]) => void>(
 
 export default function DashboardContainer() {
   const [search, setSearch] = useState<string>("");
-  const [statistics, setStatistics] = useState(KID_STATISTICS);
 
   const filterStats = useCallback((search: string) => {
     return Object.keys(KID_STATISTICS)
@@ -64,45 +63,28 @@ export default function DashboardContainer() {
       }, {} as typeof KID_STATISTICS);
   }, []);
 
-  const fetchUserStats = useCallback(async (username: string) => {
-    try {
-      const response = await fetch(
-        `http://localhost:3001/statistic/dashboard/${username}`,
-        {
-          method: "GET",
-          headers: {
-            "x-access-token": localStorage.getItem("token") || "",
-          },
-        }
-      );
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(`Error fetching statistics for user ${username}:`, error);
-      return null;
-    }
+  const debouncedSearch = useMemo(() => {
+    return debounce((search: string) => {
+      fetch(`http://localhost:3001/statistic/dashboard`, {
+        method: "GET",
+        headers: {
+          "x-access-token": localStorage.getItem("token") || "",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setSearch(data.name);
+          console.log(data);
+        });
+    }, 500);
   }, []);
 
-  const debouncedSearch = useMemo(() => {
-    return debounce(async (search: string) => {
-      const userStats = await fetchUserStats(search);
-      if (userStats) {
-        const updatedStats = { ...KID_STATISTICS };
-        Object.keys(userStats).forEach((key) => {
-          updatedStats[key].value = userStats[key];
-        });
-        setStatistics(updatedStats);
-      }
-    }, 500);
-  }, [fetchUserStats]);
-
-  useEffect(() => {
-    if (search.length > 0) {
+  const handleSearch = useCallback(
+    (search: string) => {
       debouncedSearch(search);
-    } else {
-      setStatistics(KID_STATISTICS);
-    }
-  }, [search, debouncedSearch]);
+    },
+    [debouncedSearch]
+  );
 
   const filteredStats = useMemo(
     () => filterStats(search),
@@ -110,47 +92,53 @@ export default function DashboardContainer() {
   );
 
   return (
-    <>
-      <div>
-        <div className="dashboardContainer">
-          <div className="dashboardTop">
-            <div className="dashboardTopLeft">
-              <SearchComponent
-                search={search}
-                setSearch={setSearch}
-                onSearch={debouncedSearch}
-              />
-            </div>
-            <div className="dashboardTopRight">
-              <h1 className="kidName">{search}</h1>
-            </div>
+    <div>
+      <div className="dashboardContainer">
+        <div className="dashboardTop">
+          <div className="dashboardTopLeft">
+            <SearchComponent
+              search={search}
+              setSearch={setSearch}
+              onSearch={debouncedSearch}
+            />
           </div>
-          <div className="dashboardBottom">
-            <div className="dashboardBottomLeft">
-              {Object.keys(filteredStats)
-                .slice(0, Object.keys(filteredStats).length / 2)
-                .map((key) => (
-                  <MemoizedStats
-                    key={key}
-                    name={filteredStats[key].nombre}
-                    value={filteredStats[key].value}
-                  />
-                ))}
-            </div>
-            <div className="dashboardBottomRight">
-              {Object.keys(filteredStats)
-                .slice(Object.keys(filteredStats).length / 2)
-                .map((key) => (
-                  <MemoizedStats
-                    key={key}
-                    name={filteredStats[key].nombre}
-                    value={filteredStats[key].value}
-                  />
-                ))}
-            </div>
+          <div className="dashboardTopRight">
+            <h1 className="kidName">{search}</h1>
+          </div>
+        </div>
+        <div className="dashboardBottom">
+          <div className="dashboardBottomLeft">
+            {Object.keys(KID_STATISTICS)
+              .slice(0, Object.keys(KID_STATISTICS).length / 2)
+              .map((key) => (
+                <Stats
+                  key={key}
+                  name={
+                    KID_STATISTICS[key as keyof typeof KID_STATISTICS].nombre
+                  }
+                  value={
+                    KID_STATISTICS[key as keyof typeof KID_STATISTICS].value
+                  }
+                />
+              ))}
+          </div>
+          <div className="dashboardBottomRight">
+            {Object.keys(KID_STATISTICS)
+              .slice(Object.keys(KID_STATISTICS).length / 2)
+              .map((key) => (
+                <Stats
+                  key={key}
+                  name={
+                    KID_STATISTICS[key as keyof typeof KID_STATISTICS].nombre
+                  }
+                  value={
+                    KID_STATISTICS[key as keyof typeof KID_STATISTICS].value
+                  }
+                />
+              ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
