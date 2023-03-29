bcd3ddb627a60c1f5add4c12b46f1d195adace6a
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-03-28 22:23:18 -0600

    AdminDashboard with SearchComponent works.

--- src/Components/DashboardContainer/DashboardContainer.tsx
+++ src/Components/DashboardContainer/DashboardContainer.tsx
@@ -1,42 +1,100 @@
-import React from "react";
-import './DashboardContainer.css'
+import React, { useState, useMemo, useCallback } from "react";
+import "./DashboardContainer.css";
 import { KID_STATISTICS } from "src/gameConstants";
 import Stats from "../StatsProp/Stats";
+import SearchComponent from "../SearchComponent/SearchComponent";
+
+interface IStatsProps {
+  name: string;
+  value: number;
+}
+
+const MemoizedStats = React.memo(function StatsComponent({ name, value }: IStatsProps) {
+  return <Stats name={name} value={value} />;
+});
+
+const debounce = <F extends (...args: any[]) => void>(
+  func: F,
+  delay: number
+): ((...args: Parameters<F>) => void) => {
+  let timer: NodeJS.Timeout;
+  return (...args: Parameters<F>): void => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
 
 export default function DashboardContainer() {
+  const [search, setSearch] = useState<string>("");
+  
+  const filterStats = useCallback((search: string) => {
+    return Object.keys(KID_STATISTICS)
+      .filter((key) => KID_STATISTICS[key].nombre.toLowerCase().includes(search.toLowerCase()))
+      .reduce((obj, key) => {
+        obj[key] = KID_STATISTICS[key];
+        return obj;
+      }, {} as typeof KID_STATISTICS);
+  }, []);
+  
+
+
+  const debouncedSearch = useMemo(() => {
+    return debounce((search: string) => {
+      // aqui va el query al backend para traer los datos de los niños
+    }, 500);
+  }, []);
+  
+  const handleSearch = useCallback((search: string) => {
+    debouncedSearch(search);
+  }, [debouncedSearch]);
+
+  const filteredStats = useMemo(() => filterStats(search), [filterStats, search]);
+
   return (
-    <div >
-        <div className="dashboardContainer">
-            <div className="dashboardTop">
-                top
-            </div>
-            <div className="dashboardBottom">
-                <div className="dashboardBottomLeft">
-                {
-  Object.keys(KID_STATISTICS).slice(0, Object.keys(KID_STATISTICS).length / 2).map((key) => (
-    <Stats
-      key={key}
-      name={KID_STATISTICS[key as keyof typeof KID_STATISTICS].nombre}
-      value={KID_STATISTICS[key as keyof typeof KID_STATISTICS].value}
-    />
-  ))
-}
-
-                </div>
-                <div className="dashboardBottomRight">
-                {
-  Object.keys(KID_STATISTICS).slice(Object.keys(KID_STATISTICS).length / 2).map((key) => (
-    <Stats
-      key={key}
-      name={KID_STATISTICS[key as keyof typeof KID_STATISTICS].nombre}
-      value={KID_STATISTICS[key as keyof typeof KID_STATISTICS].value}
-    />
-  ))
-}
-
-                </div>
-            </div>
+    <div>
+      <div className="dashboardContainer">
+        <div className="dashboardTop">
+          <div className="dashboardTopLeft"><SearchComponent search={search} setSearch={setSearch} onSearch={debouncedSearch}/></div>
+          <div className="dashboardTopRight">
+            <h1 className="kidName">{search}</h1>  
+          </div> 
         </div>
+        <div className="dashboardBottom">
+          <div className="dashboardBottomLeft">
+            {Object.keys(KID_STATISTICS)
+              .slice(0, Object.keys(KID_STATISTICS).length / 2)
+              .map((key) => (
+                <Stats
+                  key={key}
+                  name={
+                    KID_STATISTICS[key as keyof typeof KID_STATISTICS].nombre
+                  }
+                  value={
+                    KID_STATISTICS[key as keyof typeof KID_STATISTICS].value
+                  }
+                />
+              ))}
+          </div>
+          <div className="dashboardBottomRight">
+            {Object.keys(KID_STATISTICS)
+              .slice(Object.keys(KID_STATISTICS).length / 2)
+              .map((key) => (
+                <Stats
+                  key={key}
+                  name={
+                    KID_STATISTICS[key as keyof typeof KID_STATISTICS].nombre
+                  }
+                  value={
+                    KID_STATISTICS[key as keyof typeof KID_STATISTICS].value
+                  }
+                />
+              ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/SearchComponent/SearchComponent.tsx
+++ src/Components/SearchComponent/SearchComponent.tsx
@@ -1,1 +1,67 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import "./SearchComponent.css";
+
+interface SearchComponentProps {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+  onSearch: (query: string) => void;
+}
+
+export default function SearchComponent({
+  search,
+  setSearch,
+  onSearch,
+}: SearchComponentProps): JSX.Element {
+  const debounce = useCallback(
+    <F extends (...args: any[]) => void>(
+      func: F,
+      delay: number
+    ): ((...args: Parameters<F>) => void) => {
+      let timer: NodeJS.Timeout;
+      return (...args: Parameters<F>): void => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          func(...args);
+        }, delay);
+      };
+    },
+    []
+  );
+
+  const debouncedSearch = useCallback(
+    debounce(() => {
+      onSearch(search);
+    }, 500),
+    [debounce, onSearch, search]
+  );
+
+  return (
+    <div>
+      <div className="searchContainer">
+        <div className="SearchLabelContainer">
+          <label htmlFor="searchInput" className="SearchLabel">
+            Ingrese nombre del niño:{" "}
+          </label>
+        </div>
+        <div className="SearchLabelContainer">
+          <input
+            type="text"
+            id="searchInput"
+            className="searchInput"
+            placeholder="Ingrese su búsqueda"
+            value={search}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
+              setSearch(e.target.value);
+              debouncedSearch();
+            }}
+          />
+        </div>
+        <div className="SearchButtonContainer">
+          <button className="SearchButton" onClick={() => onSearch(search)}>
+            Buscar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
