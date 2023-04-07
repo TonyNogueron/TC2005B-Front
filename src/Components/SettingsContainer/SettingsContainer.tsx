48642bc29564abf9c25f24fcbeb73f9bed39155a
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-04-06 20:05:43 -0600

    SettingsPage

---
+++ src/Components/SettingsContainer/SettingsContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SettingsContainer.css";
+
+export default function SettingsContainer() {
+    return (
+        <div className="settings-container">
+            <div className="settings-header">
+                <h1>Settings</h1>
+            </div>
+            <div className="settings-content">
+            </div>
+        </div>
+    );
+}
--- src/pages/LeaderboardPage/Leaderboard.tsx
+++ src/pages/LeaderboardPage/Leaderboard.tsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Leaderboard.css";
-import Header from "src/Components/HeaderProp/Header";
-import Footer from "src/Components/FooterProp/Footer";
+import HeaderComponent from "src/Components/HeaderComponent/HeaderComponent";
 import { HEADER_ITEMS, HeaderConstant,LINKS,LOGO_CONSTANTS,LogoConstant,PAGE_TITLE } from "../../constants";
 import BackgroundProp from "src/Components/BackgroundProp/BackgroundProp";
 import { useNavigate } from "react-router-dom";
 
 export default function Leaderboard() {
+    const logo: LogoConstant[] = LOGO_CONSTANTS;
+    const menuItems: HeaderConstant[] = HEADER_ITEMS;
+    const [isMenuOpen, setIsMenuOpen] = useState(true); // add state for isMenuOpen
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+      };
+
     return(
+        
         <div className="leaderboard">
-            <BackgroundProp backgroundName="blue-background" />
+            <BackgroundProp backgroundName="white-background" />
+            <HeaderComponent
+            logo={logo[0]}
+            menuItems={menuItems}
+            isAuthenticated={true}
+            isAdmin={true}
+            onMenuToggle={handleMenuToggle}
+        />
 
-
-            <Footer title={PAGE_TITLE} logo={LOGO_CONSTANTS[0]} menuItems={HEADER_ITEMS} />
             
         </div>
     );
